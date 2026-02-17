91f12e8bfb6a070b88dcc06f39fce810575e16ae
Ahmed Ben Amor <bnamor.ahmad@gmail.com>
2026-02-17 15:07:45 +0100

    Section 35 : ajout fichier json dans le projet liste des courses & ajout gitignore

--- liste_de_courses/liste_de_courses.py
+++ liste_de_courses/liste_de_courses.py
@@ -1,4 +1,14 @@
-liste_des_courses = []
+import json
+import os
+
+CUR_DIR = os.path.dirname(__file__)
+chemin = os.path.join(CUR_DIR, "liste_des_courses.json")
+
+if not os.path.exists(chemin):
+    with open(chemin, "w", encoding="utf-8") as f:
+        json.dump([], f)
+
+
 liste_des_choix = ["Ajouter un élément à la liste de courses",
                    "Retirer un élément de la liste de courses",
                    "Afficher les éléments de la liste de courses",
@@ -18,27 +28,55 @@ while a :
 
     if choix == "1" :
         demande = input("Ajouter un élément à la liste de courses : ")
-        liste_des_courses.append(demande)
+
+        # récupération des données dans la variable donnees
+        with open(chemin, "r", encoding='UTF-8') as f :
+            donnees = json.load(f)
+        # modifier les donnees dans la variable donnees
+        donnees.append(demande)
+        # réécrire les donnees dans notre dichier JSON
+        with open(chemin, "w", encoding='UTF-8') as f :
+            json.dump(donnees, f, indent=4)
+
         for i, element in enumerate(liste_des_choix, start=1) :
             print(i, element)
         choix = input("Votre choix : ")
     
     elif choix == "2" :
         demande = input("Retirer un élément de la liste de courses : ")
-        liste_des_courses = [x for x in liste_des_courses if x != demande]
+        
+
+        # récupération des données dans la variable donnees
+        with open(chemin, "r", encoding='UTF-8') as f :
+            donnees = json.load(f)
+        # modifier les donnees dans la variable donnees
+        donnees = [x for x in donnees if x != demande]
+        # réécrire les donnees dans notre dichier JSON
+        with open(chemin, "w", encoding='UTF-8') as f :
+            json.dump(donnees, f, indent=4)
+
         for i, element in enumerate(liste_des_choix, start=1) :
             print(i, element)
         choix = input("Votre choix : ")
         
     elif choix == "3" :
-        for x, valeur in enumerate(liste_des_courses, start=1) :
-            print(x, valeur)
+
+        with open(chemin, "r", encoding='UTF-8') as f :
+            contenu = json.load(f)
+            
+        print("--------------------------------------------------------------------------------")
+        for i, element in enumerate(contenu, start=1) :
+            print(f"{i}. {element}")
+        print("--------------------------------------------------------------------------------")
+
         for i, element in enumerate(liste_des_choix, start=1) :
             print(i, element)
         choix = input("Votre choix : ")
         
     elif choix == "4" :
-        liste_des_courses.clear()
+        with open(chemin, "w", encoding="utf-8") as f:
+            json.dump([], f)
+
         for i, element in enumerate(liste_des_choix, start=1) :
             print(i, element)
         choix = input("Votre choix : ")
--- test.py
+++ test.py
@@ -1,50 +1,86 @@
-import random
- 
-vie_joueur = 50
-vie_ennemi = 50
-potion = 3
- 
-while vie_joueur > 0 and vie_ennemi > 0:
-    vie = random.randint(15, 50)
-    degat_joueur = random.randint(5, 10)
-    degat_ennemi = random.randint(5, 15)
-    choix = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) ")
-    while not choix.isdigit() or int(choix) not in range(1, 3):
-        choix = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) ")
-    if int(choix) == 1 :
-        print(f"Vous avez infligé {degat_joueur} points de dégats à l'ennemi")
-        print(f"L'ennemi vous a infligé {degat_ennemi} points de dégats")
-        print(f"Il vous reste {vie_joueur - degat_ennemi} points de vie")
-        print(f"Il reste {vie_ennemi - degat_joueur} points de vie à l'ennemi")
-        vie_joueur = vie_joueur - degat_ennemi
-        vie_ennemi = vie_ennemi - degat_joueur
-        print("---------------------------------------------------------------------------")
+import json
+import os
+
+CUR_DIR = os.path.dirname(__file__)
+chemin = os.path.join(CUR_DIR, "liste_des_courses.json")
+
+if not os.path.exists(chemin):
+    with open(chemin, "w", encoding="utf-8") as f:
+        json.dump([], f)
+
+
+liste_des_choix = ["Ajouter un élément à la liste de courses",
+                   "Retirer un élément de la liste de courses",
+                   "Afficher les éléments de la liste de courses",
+                   "Vider la liste de courses",
+                   "Quitter le programme"]
+
+for i, element in enumerate(liste_des_choix, start=1) :
+    print(i, element)
+
+choix = input("Votre choix : ")
+a = True
+while a :
+    while not choix.isdigit() or int(choix) not in range(1,6) :
+        for i, element in enumerate(liste_des_choix, start=1) :
+            print(i, element)
+        choix = input("Votre choix : ")
+
+    if choix == "1" :
+        demande = input("Ajouter un élément à la liste de courses : ")
+
+        # récupération des données dans la variable donnees
+        with open(chemin, "r", encoding='UTF-8') as f :
+            donnees = json.load(f)
+        # modifier les donnees dans la variable donnees
+        donnees.append(demande)
+        # réécrire les donnees dans notre dichier JSON
+        with open(chemin, "w", encoding='UTF-8') as f :
+            json.dump(donnees, f, indent=4)
+
+        for i, element in enumerate(liste_des_choix, start=1) :
+            print(i, element)
+        choix = input("Votre choix : ")
+    
+    elif choix == "2" :
+        demande = input("Retirer un élément de la liste de courses : ")
+        
+
+        # récupération des données dans la variable donnees
+        with open(chemin, "r", encoding='UTF-8') as f :
+            donnees = json.load(f)
+        # modifier les donnees dans la variable donnees
+        donnees = [x for x in donnees if x != demande]
+        # réécrire les donnees dans notre dichier JSON
+        with open(chemin, "w", encoding='UTF-8') as f :
+            json.dump(donnees, f, indent=4)
+
+        for i, element in enumerate(liste_des_choix, start=1) :
+            print(i, element)
+        choix = input("Votre choix : ")
+        
+    elif choix == "3" :
+
+        with open(chemin, "r", encoding='UTF-8') as f :
+            contenu = json.load(f)
+            
+        print("--------------------------------------------------------------------------------")
+        for i, element in enumerate(contenu, start=1) :
+            print(f"{i}. {element}")
+        print("--------------------------------------------------------------------------------")
+
+        for i, element in enumerate(liste_des_choix, start=1) :
+            print(i, element)
+        choix = input("Votre choix : ")
+        
+    elif choix == "4" :
+        with open(chemin, "w", encoding="utf-8") as f:
+            json.dump([], f)
+
+        for i, element in enumerate(liste_des_choix, start=1) :
+            print(i, element)
+        choix = input("Votre choix : ")
+        
     else :
-        if (potion - 1) >= 0 :
-            if (potion - 1) > 1 :
-                print(f"Vous récupérez {vie} points de vie ({potion - 1} potions restantes)")
-            else :
-                print(f"Vous récupérez {vie} points de vie ({potion - 1} potion restantes)")
-            vie_joueur = vie_joueur + vie
-            potion -= 1
-            print(f"L'ennemi vous a infligé {degat_ennemi} points de dégats")
-            print(f"Il vous reste {vie_joueur - degat_ennemi} points de vie")
-            print(f"Il reste {vie_ennemi} points de vie à l'ennemi")
-            vie_joueur = vie_joueur - degat_ennemi
-            print("---------------------------------------------------------------------------")
-            print("Vous passez votre tour ...")
-            degat_ennemi = random.randint(5, 15)
-            print(f"L'ennemi vous a infligé {degat_ennemi} points de dégats")
-            print(f"Il vous reste {vie_joueur - degat_ennemi} points de vie")
-            print(f"Il reste {vie_ennemi} points de vie à l'ennemi")
-            vie_joueur = vie_joueur - degat_ennemi
-            print("---------------------------------------------------------------------------")
-        else :
-            print("Vous n'avez plus de potions ...")
-    if vie_joueur <= 0 :
-        print("Tu as perdu :'(")
-        break
-    elif vie_ennemi <= 0 :
-        print("Vous avez gagné :D")
-        break
-print("Fin de jeu.")
+        print("A bientôt !")
+        a = False
